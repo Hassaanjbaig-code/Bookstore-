553f9c9b2ecb8a2d5a6cc125fc1392fbb8cdfd38
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-01-20 12:28:27 +0500

    Add Header

---
+++ src/Component/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Header.css';
+
+const Header = () => {
+    const links = [
+        {
+            id: 1,
+            path: '/',
+            text: 'Book'
+        },
+        {
+            id: 1,
+            path: '/Categories',
+            text: 'Categories'
+        }
+    ]
+  return (
+    <nav className="nav-bar">
+      <ul className="nav-menu">
+        <h3>Bookstore CMS</h3>
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink to={link.path} className="nav-item">
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Header

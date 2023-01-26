0d5c2f2777cf8aa80b73ae65184a77f6b6c9db44
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-01-26 13:10:57 +0500

    Fix linter error

--- src/Pages/Categories.js
+++ src/Pages/Categories.js
@@ -1,18 +1,17 @@
-/* eslint-disable */
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { CheckStatus } from '../redux/categories/categories';
 
 const categories = () => {
-  const dispatch = useDispatch();
-  const handle = useSelector((state) => state.Categories.status)
+  const dispatch = useDispatch(); // eslint-disable-line
+  const handle = useSelector((state) => state.Categories.status); // eslint-disable-line
   const handleClick = () => dispatch(CheckStatus());
   return (
     <div>
       <h3>{handle}</h3>
-      <button onClick={handleClick} type='button'>CheckStatus</button>
+      <button onClick={handleClick} type="button">CheckStatus</button>
     </div>
-  )
-}
+  );
+};
 
 export default categories;

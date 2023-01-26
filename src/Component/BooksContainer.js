ba8c9ae1c035ec679231c1804545cc12c304c5ea
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-01-26 10:22:20 +0500

    Remove and add

--- src/Component/BooksContainer.js
+++ src/Component/BooksContainer.js
@@ -4,29 +4,28 @@ import Booklist from './Booklist';
 import Formbook from './Formbook';
 
 const BooksContainer = () => {
-  const store = [
-    {
-      id: 1,
-      bookcategory: 'Action',
-      Title: 'The Hunger Games',
-      Author: 'Suzanne Collins',
-    },
-    {
-      id: 2,
-      bookcategory: 'Science Fiction',
-      Title: 'Dune',
-      Author: 'Frank Herbert',
-    },
-    {
-      id: 3,
-      bookcategory: 'Economy',
-      Title: 'Capital in the Twenty-First Century',
-      Author: 'Suzanne Collins',
-    },
-  ];
+  // const store = [
+  //   {
+  //     id: 1,
+  //     bookcategory: 'Action',
+  //     Title: 'The Hunger Games',
+  //     Author: 'Suzanne Collins',
+  //   },
+  //   {
+  //     id: 2,
+  //     bookcategory: 'Science Fiction',
+  //     Title: 'Dune',
+  //     Author: 'Frank Herbert',
+  //   },
+  //   {
+  //     id: 3,
+  //     bookcategory: 'Economy',
+  //     Title: 'Capital in the Twenty-First Century',
+  //     Author: 'Suzanne Collins',
+  //   },
+  // ];
 
   const book = useSelector((state) => state.Book);
-  // const { id, Title, Author } = store
   return (
     <>
       <ul>
